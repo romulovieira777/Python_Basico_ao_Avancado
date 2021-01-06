b0f2fcca712074e2ae2fea55000979f043b4db78
romulovieira777 <romulo.vieira777@gmail.com>
2021-01-06 18:21:18 -0300

    Tipos Primitivos

---
+++ Seção 02 - Tipos Primitivos/variavel.py
@@ -0,0 +1,11 @@
+# Declarando Quatro Variáveis
+student_age = 10
+student_result = 85.7
+disapproved = False
+student_name = 'Felicity Smoak'
+
+# Apresentando os Valores na Tela
+print(student_age)
+print(student_result)
+print(disapproved)
+print(student_name)
