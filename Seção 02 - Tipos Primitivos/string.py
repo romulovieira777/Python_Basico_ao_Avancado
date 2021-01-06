2e612cc073f40ac3dd3f735515a21c9203266017
romulovieira777 <romulo.vieira777@gmail.com>
2021-01-06 18:29:16 -0300

    Tipos Primitivos

---
+++ Seção 02 - Tipos Primitivos/string.py
@@ -0,0 +1,14 @@
+# Declarando uma variável
+our_text = 'Python is the best language'
+
+# Apresentando o valor na tela do tamanho do texto na variável
+print(len(our_text))
+
+# Apresentado na Tela partes do texto da variável
+print(our_text[0])
+print(our_text[3])
+print(our_text[-1])
+print(our_text[0:3])
+print(our_text[0:])
+print(our_text[:3])
+print(our_text[:])
